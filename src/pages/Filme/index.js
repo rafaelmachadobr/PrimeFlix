de258cf853061a7a24b375588f03324d192fcd1a
Rafael <rafaelf.machado2004@gmail.com>
2022-12-02 11:35:37 -0300

    Personalizando alertas

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState }  from 'react';
 import { useParams, useNavigate } from 'react-router-dom'
 import './filme-info.css';
 import api from '../../services/api'
+import { toast } from 'react-toastify'
 
 
 function Filme() {
@@ -45,13 +46,13 @@ function Filme() {
         const hasFilme = filmesSalvos.some( (filmeSalvo) => filmeSalvo.id === filme.id )
 
         if(hasFilme){
-            alert("Esse filme já está na lista")
+            toast.warn("Esse filme já está na sua lista!")
             return
         }
 
         filmesSalvos.push(filme)
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
-        alert("Filme salvo com sucesso")
+        toast.success("Filme salvo com sucesso!")
     }
 
     if(loading){
